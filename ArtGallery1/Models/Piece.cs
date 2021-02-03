c1fe24c66de1f0d9d9407cc593f081df1ea1f752
ntrpi <kupfer.sandra.m@gmail.com>
2021-02-02 20:47:56 -0500

    Add project files.

---
+++ ArtGallery1/Models/Piece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtGallery1.Models
+{
+    public class Piece
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
